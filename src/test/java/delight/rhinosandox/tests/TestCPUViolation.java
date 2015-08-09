d6487ac4ce59afb828cf420d611f72be066ae047
mxro <mxro@nowhere.com>
2015-08-09 21:00:20 +1000

    Implementing Rhino Sandbox Autocommit Sun Aug 09 21:00:20 EST 2015

--- src/test/java/delight/rhinosandox/tests/TestCPUViolation.java
+++ src/test/java/delight/rhinosandox/tests/TestCPUViolation.java
@@ -1,10 +1,15 @@
 package delight.rhinosandox.tests;
 
+import delight.rhinosandox.RhinoSandbox;
+import delight.rhinosandox.RhinoSandboxes;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestCPUViolation {
   @Test
   public void test() {
+    final RhinoSandbox sandbox = RhinoSandboxes.create();
+    sandbox.setInstructionLimit(50000);
+    sandbox.eval("while (true) { };");
   }
 }
--- src/test/xtend/delight/rhinosandox/tests/TestCPUViolation.xtend
+++ src/test/xtend/delight/rhinosandox/tests/TestCPUViolation.xtend
@@ -1,13 +1,18 @@
 package delight.rhinosandox.tests
 
 import org.junit.Test
+import delight.rhinosandox.RhinoSandboxes
 
 class TestCPUViolation {
 	
 	@Test
 	def void test() {
 		
+		val sandbox = RhinoSandboxes.create
 		
+		sandbox.instructionLimit = 50000
+		
+		sandbox.eval("while (true) { };")
 		
 	}
 	

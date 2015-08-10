6857870c4365d456a5ef14fb1f57f8f7c827bee8
mxro <mxro@nowhere.com>
2015-08-10 12:24:31 +1000

    Implementing Rhino Sandbox Autocommit Mon Aug 10 12:24:31 EST 2015

--- src/main/java/delight/rhinosandox/internal/SafeNativeJavaObject.java
+++ src/main/java/delight/rhinosandox/internal/SafeNativeJavaObject.java
@@ -1,5 +1,17 @@
 package delight.rhinosandox.internal;
 
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.mozilla.javascript.Scriptable;
+
 @SuppressWarnings("all")
 public class SafeNativeJavaObject {
+  public SafeNativeJavaObject(final Scriptable scope, final Object javaObject, final Class<?> staticType) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInvalid number of arguments. The constructor Object() is not applicable for the arguments (Scriptable,Object,Class<?>)");
+  }
+  
+  /* @Override
+   */public Object get;
+  
+  private Procedure1<? super String> name;
 }
--- src/main/xtend/delight/rhinosandox/internal/SafeNativeJavaObject.xtend
+++ src/main/xtend/delight/rhinosandox/internal/SafeNativeJavaObject.xtend
@@ -1,5 +1,18 @@
 package delight.rhinosandox.internal
 
+import org.mozilla.javascript.Scriptable
+
 class SafeNativeJavaObject {
-	
+	 new(Scriptable scope, Object javaObject,
+                                   Class<?> staticType) {
+        super(scope, javaObject, staticType)
+    }
+
+    @Override
+    public Object get(String name, Scriptable start) {
+       // if ("getClass".equals(name)) {
+       //     return NOT_FOUND;
+      //  }
+        return super.get(name, start);
+    }
 }
